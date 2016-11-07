303a15d38ddf01fe41112bde49c06368aba087ec
Chris Saenz <chris@saenzmail.net>
2016-11-06 21:03:59 -0500

    Adding first clojure learning project. Yay for shiny new repository!

---
+++ clojure-apps/clojure-noob/src/clojure_noob/core.clj
@@ -0,0 +1,111 @@
+(ns  clojure-noob.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+)
+
+(def hobbit                 [{:name "head" :size 3}
+                             {:name "left-eye" :size 1}
+                             {:name "left-ear" :size 1}
+                             {:name "mouth" :size 1}
+                             {:name "nose" :size 1}
+                             {:name "neck" :size 2}
+                             {:name "left-shoulder" :size 3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest" :size 10}
+                             {:name "back" :size 10}
+                             {:name "left-forearm" :size 3}
+                             {:name "abdomen" :size 6}
+                             {:name "left-kidney" :size 1}
+                             {:name "left-hand" :size 2}
+                             {:name "left-knee" :size 2}
+                             {:name "left-thigh" :size 4}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "left-achilles" :size 1}
+                             {:name "left-foot" :size 2}
+                             ]
+)
+
+
+
+
+(defn matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+(defn better-symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (matching-part part)])))
+          []
+          asym-body-parts))  
+
+(defn random-answer [choices]
+ (fn [] (get choices (rand-int (count choices)))))
+
+(def jub (random-answer [
+                  "Awesome" 
+                  "Incredible" 
+                  "Amazing" 
+                  "Wow" 
+                  "Mind blown" 
+                  "Goodness gracious" 
+                  "Crack shot" 
+                  "Genius" 
+                  "Zounds"
+                  "Holy Cow"
+                  "Egads"
+                  "Sufferin' Succotach"
+                  "Great Horny Toads"
+                  "Blue Blistering Barnacles"
+                  "Oh look"
+                  "Yikes"
+                  "Yeeoww"
+                  "Crack"
+                  "Boom"
+                  "Thud"
+                  "Zing"
+                  "Plunk"
+                  "Holy Smokes"
+                  "Good grief Charlie Brown"
+                  "Golly"
+                  "Ohmigosh"
+                 "Impressive"
+]))
+
+(def hobbit-name (random-answer [
+                  "Sam"
+                  "Frodo"
+                  "Bilbo"
+                  "Rosy"
+                  "Merry"
+                  "Pippin"
+                  "Gaffer"
+                  "Gandalf"
+                  "Aragorn"
+                  "Legolas"
+                  "Gimli"
+                  "Saruman"
+                  "Elizabeth"
+ 
+]))
+
+
+
+(defn hit
+  [asym-body-parts]
+  (let [sym-parts (better-symmetrize-body-parts asym-body-parts)
+        body-part-size-sum (reduce + (map :size sym-parts))
+        target (rand body-part-size-sum)]
+     (loop [[part & remaining] sym-parts
+           accumulated-size (:size part)]
+      (if (> accumulated-size target)
+        (do
+          (println (str (jub) "! You hit " (hobbit-name) "'s " (:name part) "!"))
+          part
+          )
+        (recur remaining (+ accumulated-size (:size (first remaining))))))))
