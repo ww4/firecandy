71e11e6c01be735f5f81798738b23936b1fc8f85
Chris Saenz <chris@saenzmail.net>
2016-11-30 22:49:06 -0500

    First draft of database, mostly finished book excercises.

--- clojure-apps/fwpd/src/fwpd/core.clj
+++ clojure-apps/fwpd/src/fwpd/core.clj
@@ -1,7 +1,66 @@
-(ns fwpd.core
-  (:gen-class))
+(ns fwpd.core)
+(def filename "suspects.csv")
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+
+(def vamp-keys [:name :glitter-index])
+
+(defn str->int
+  [str]
+  (Integer. str)
+  )
+
+(def conversions {:name identity
+                  :glitter-index str->int})
+
+(defn convert
+  [vamp-key value]
+((get conversions vamp-key) value)
+)
+
+(defn parse
+"Convert a CSV string into rows of columns"
+[string]
+(map #(clojure.string/split % #",")
+     (clojure.string/split string #"\n"))
+  )
+
+(defn mapify
+"Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10"
+[rows]
+(map (fn [unmapped-row]
+       (reduce (fn [row-map [vamp-key value]]
+                 (assoc row-map vamp-key (convert vamp-key value)))
+               {}
+               (map vector vamp-keys unmapped-row)))
+       rows)
+)
+
+(defn get-records
+  []
+  (mapify (parse (slurp filename)))
+  )
+
+(defn glitter-filter
+  [minimum-glitter records]
+  (filter #(>= (:glitter-index %) minimum-glitter) records)
+  )
+
+(defn name-search
+  [min]
+  (map :name (glitter-filter min (get-records))))
+
+(defn append-suspect
+  [suspect glitz]
+  (save-csv (cons {:name suspect :glitter-index glitz} (get-records)) "" )
+  )
+
+
+(defn save-csv [input output]
+  (if (empty? input)
+    (spit filename output)
+    (save-csv (rest input)
+              (str  output
+                    (:name (first input)) ","
+                    (:glitter-index (first input)) "\n"
+)
+)))
