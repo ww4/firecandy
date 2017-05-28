e082eb3cc11386744473936f138fbd1f868dd295
ww4 <chris@saenzmail.net>
2017-05-28 11:45:25 -0400

    Latest changes; added most of the functions

--- clojure-apps/summarywriter/project.clj
+++ clojure-apps/summarywriter/project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+		 [semantic-csv "0.2.1-alpha1"]
+		 [hiccup "2.0.0-alpha1"]]
   :main ^:skip-aot summarywriter.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
