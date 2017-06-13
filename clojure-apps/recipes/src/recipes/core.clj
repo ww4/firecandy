81b7c6d9e7112e5aab6e88b634a0488a7df7ed11
Chris Saenz <chris@saenzmail.net>
2017-06-13 15:13:48 -0400

    working on recipe app

--- clojure-apps/recipes/src/recipes/core.clj
+++ clojure-apps/recipes/src/recipes/core.clj
@@ -17,41 +17,32 @@
          :user "chris"
          :password "recipe"})
 
-(def family-recipes  ;;initial template for data structure
-  {:ID123
-   {:title "macaroni"
-    :ingredients
-    [{:ing-name "noodles" :unit "cup" :qty 2}
-     {:ing-name "cheese" :unit "cup" :qty 3}]
-      :instructions
-       ["cook noodles"
-        "add cheese"
-        "enjoy"]}
-   :ID101
-   {:title "Skillet Chicken Macaroni"
-    :ingredients
-    [{:ing-name "butter" :unit "stick" :qty 1}
-     {:ing-name "elbow macaroni" :unit "cup" :qty 6}
-     {:ing-name "grated cheddar cheese" :unit "handful" :qty 1}
-     {:ing-name "onion" :unit "" :qty 2}
-     {:ing-name "diced cooked chicken" :unit "cup" :qty 2}
-     {:ing-name "tomato sauce" :unit "15 oz can" :qty 3}]
-    :instructions
-    ["Sauté onion in butter in very large skillet." "Mix dry macaroni in butter and onion till coated and yellowed." "Add tomato sauce and water and seasonings." "Cover and simmer 15 minutes, stirring occasionally. Add water if needed." "Add chicken that has been pulled apart.  Cook 5 minutes more." "Cover with grated cheese and simmer until cheese melts."]
-    :notes "Serves 14-16. This is tripled from the original recipe."}
-   })
+(def sample-recipe  ;;recipe data structure
+  {:title "Skillet Chicken Macaroni"
+   :notes "Serves 14-16. This is tripled from the original recipe."
+   :ingredients
+   [{:ing-name "butter" :unit "stick" :qty 1}
+    {:ing-name "elbow macaroni" :unit "cup" :qty 6}
+    {:ing-name "grated cheddar cheese" :unit "handful" :qty 1}
+    {:ing-name "onion" :unit "" :qty 2}
+    {:ing-name "diced cooked chicken" :unit "cup" :qty 2}
+    {:ing-name "tomato sauce" :unit "15 oz can" :qty 3}]
+   :instructions
+   ["Sauté onion in butter in very large skillet." "Mix dry macaroni in butter and onion till coated and yellowed." "Add tomato sauce and water and seasonings." "Cover and simmer 15 minutes, stirring occasionally. Add water if needed." "Add chicken that has been pulled apart.  Cook 5 minutes more." "Cover with grated cheese and simmer until cheese melts."]})
 
 (defn save-recipe [recipe db]
-  ;;takes a map structure like output of get-recipe
-  ;;check for ingredient, add to ingredient table
-  ;;increment last recipe id
-  (let [newid (inc (:max (first (sql/query db "SELECT MAX(id) FROM recipe"))))]
-    (sql/insert! db :recipe {:title (:title recipe)
-                             :id newid})
-    (sql/insert! db :instructions (for [instruction (:instructions recipe)]
-                      {:recipe_id newid, :step_number n :instruction instruction} )) ;;get counter working
-    (sql/insert! db :ing_qty (for [ingredient (:ingredients recipe)]
-                      ))))
+  ;; takes a data structure like sample-recipe
+  ;; check for ingredient, add to ingredient table
+  ;; increment last recipe id
+  (let [{:keys [title notes ingredients instructions]} recipe]
+    (let [newid (inc (:max (first (sql/query db "SELECT MAX(id) FROM recipe"))))]
+      (sql/insert! db :recipe { :title title
+                               :notes notes
+                               :id newid})
+      (sql/insert! db :instructions (for [instruction instructions]
+                                      {:recipe_id newid :step_number n :instruction instruction} ))
+      (sql/insert! db :ing_qty (for [ingredient ingredients]
+                                 )))))
 
 (defn get-recipe [id db]
   (merge
