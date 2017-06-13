75d23e1133fa626affaf68bedec27ddde8b6cb3b
Chris Saenz <chris@saenzmail.net>
2017-06-12 22:00:26 -0400

    renamed emacs.d directories

--- .emacs.d/elpa/ido-vertical-mode-20160429.1037/ido-vertical-mode-autoloads.el
+++ .emacs.d/elpa/ido-vertical-mode-20160429.1037/ido-vertical-mode-autoloads.el
@@ -1,16 +1,15 @@
 ;;; ido-vertical-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "ido-vertical-mode" "ido-vertical-mode.el"
-;;;;;;  (22827 29258 216917 905000))
+;;;### (autoloads (ido-vertical-mode) "ido-vertical-mode" "ido-vertical-mode.el"
+;;;;;;  (22846 60750 854991 126000))
 ;;; Generated autoloads from ido-vertical-mode.el
 
 (defvar ido-vertical-mode nil "\
 Non-nil if Ido-Vertical mode is enabled.
-See the `ido-vertical-mode' command
-for a description of this minor mode.
+See the command `ido-vertical-mode' for a description of this minor mode.
 Setting this variable directly does not take effect;
 either customize it (see the info node `Easy Customization')
 or call the function `ido-vertical-mode'.")
@@ -24,9 +23,16 @@ Makes ido-mode display vertically.
 
 ;;;***
 
+;;;### (autoloads nil nil ("ido-vertical-mode-pkg.el") (22846 60750
+;;;;;;  871551 800000))
+
+;;;***
+
+(provide 'ido-vertical-mode-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; ido-vertical-mode-autoloads.el ends here
